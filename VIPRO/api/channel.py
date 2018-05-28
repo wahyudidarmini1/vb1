e41ea107fb0a3bd2c93609739d5d072c8fa6a2d6
wahyudidarmini1 <37975743+wahyudidarmini1@users.noreply.github.com>
2018-05-28 10:30:29 +0700

    Update channel.py

--- VIPRO/api/channel.py
+++ VIPRO/api/channel.py
@@ -19,7 +19,8 @@ class Channel:
     channel_query_path = "/CH4"
 
     UA = "Line/7.18.0"
-    LA = "CHROMEOS\t7.18.0\tiVipro\t11.12.1"    
+    #LA = "CHROMEOS\t7.18.0\tiVipro\t11.12.1"
+    LA = "IOSIPAD\t7.14.0\tiPhone OS\t10.12.0"
 
     authToken = None
     mid = None
