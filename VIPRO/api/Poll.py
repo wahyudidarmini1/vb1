323448971dd18bba635621852b0697bd87434645
wahyudidarmini1 <37975743+wahyudidarmini1@users.noreply.github.com>
2018-05-28 10:22:00 +0700

    Add files via upload

--- VIPRO/api/Poll.py
+++ VIPRO/api/Poll.py
@@ -18,8 +18,9 @@ class Poll:
   host = "gd2.line.naver.jp";
   port = 443;
 
-  UA = "Line/7.18.0"
-  LA = "CHROMEOS\t7.18.0\tiVipro\t11.12.1"      
+  UA = "Line/1.4.17"
+#  LA = "IOSIPAD\x097.14.0\x09iPhone_OS\x0910.12.0"
+  LA = "CHROMEOS\t1.4.17\tChrome_OS\t1"
 
   rev = 0
 
